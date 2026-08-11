a75033803324139019868647888ffe36904cd95c
Jake Snow <97jake@gmail.com>
2026-08-10 23:56:56 -0400

    big overhaul of design and home page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Jake's Spot";
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jake Snow';
+export const SITE_DESCRIPTION =
+	'AI/ML pipeline development, AI strategy consulting, and full-stack software development.';
